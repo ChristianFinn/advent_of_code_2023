60717d5a59447c1a2adfc29e8ba57a3356716377
christian.finn@outlook.com <christian.finn@bjss.com>
2023-12-03 13:11:59 +0000

    Star 2

--- day1/main.js
+++ day1/main.js
@@ -1,10 +1,3 @@
-console.log("Start")
-// 14gxqgqsqqbxfpxnbccjc33eight
-// eight2sevenkl
-// mrjstg5onetwoeightgcczx8vgrgl
-// 9246
-// ninetwo2crrqk2grsctqxqbcrmrdsqbrz9eight
-
 async function main(){
     var lines = (await readCalibrationInput()).split('\n');
     var total = 0
@@ -13,13 +6,38 @@ for(var i = 0;i < lines.length;i++){
     var items = lines[i];
     
     for(var j = 0;j < lines[i].length;j++){     
+        if (items.slice(j, j+3) == "one"){
+            numbers_in_line.push("1")
+        }
+        if (items.slice(j, j+3) == "two"){
+            numbers_in_line.push("2")
+        }
+        if (items.slice(j, j+5) == "three"){
+            numbers_in_line.push("3")
+        }
+        if (items.slice(j, j+4) == "four"){
+            numbers_in_line.push("4")
+        }
+        if (items.slice(j, j+4) == "five"){
+            numbers_in_line.push("5")
+        }
+        if (items.slice(j, j+3) == "six"){
+            numbers_in_line.push("6")
+        }
+        if (items.slice(j, j+5) == "seven"){
+            numbers_in_line.push("7")
+        }
+        if (items.slice(j, j+5) == "eight"){
+            numbers_in_line.push("8")
+        }
+        if (items.slice(j, j+4) == "nine"){
+            numbers_in_line.push("9")
+        }
         if (!isNaN(items[j]) && items[j] != "\r") {
             numbers_in_line.push(items[j]);
           }
     }
-    console.log(numbers_in_line)
     var double_number = numbers_in_line[0] + numbers_in_line[numbers_in_line.length-1]
-    console.log(double_number)
     total += Number(double_number)
 }
 console.log(total)
@@ -30,7 +48,4 @@ async function readCalibrationInput() {
     return await response.text();
   }
 
-
 main()
-
-console.log("Finish")
